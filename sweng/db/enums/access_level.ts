c4f5883b564d5e6ff62ffd9bb4b36401eab46359
Althea <thealeanne23@gmail.com>
2026-06-11 18:19:31 +0800

    feat: add array for non super admin roles (for testing)

--- sweng/db/enums/access_level.ts
+++ sweng/db/enums/access_level.ts
@@ -8,3 +8,20 @@ export const access_level = pgEnum('access_level',
         'super_admin',
         'staff_admin'
     ]);
+
+
+export const ALL_ROLES = [
+  'donor',
+  'onsite_admin',
+  'med_prof',
+  'director',
+  'super_admin',
+  'staff_admin',
+] as const;
+
+export type Role = typeof ALL_ROLES[number];
+export type NonSuperAdminRole = Exclude<Role, 'super_admin'>;
+
+export const NON_SUPER_ADMIN_ROLES = ALL_ROLES.filter(
+    role => role !== 'super_admin'
+) as Exclude<Role, 'super_admin'>[];
