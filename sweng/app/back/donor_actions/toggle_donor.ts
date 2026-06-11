53dad5cb0c3303b0ffabf9530c1e823338403740
Hatzious <avtiu29@gmail.com>
2026-06-11 20:56:29 +0800

    refactor: uncommented super admin verification on donor deletion

--- sweng/app/back/donor_actions/toggle_donor.ts
+++ sweng/app/back/donor_actions/toggle_donor.ts
@@ -9,7 +9,7 @@ import { donor } from "@/db/models/donor";
 export type DeletionMode = 'soft' | 'hard' | 'reactivate';
 
 export async function deleteDonors (donorlist: (number | string)[], command: DeletionMode) {
-    {/* const supabase = await serverSupa();
+    { const supabase = await serverSupa();
         const { data: { user } } = await supabase.auth.getUser();
     
         if (!user) {
@@ -40,22 +40,6 @@ export async function deleteDonors (donorlist: (number | string)[], command: Del
                     return { success: false, message: err.message };
                 }
             }
-        }*/
-
-        try {
-                    const idList = donorlist.map((item) => BigInt(item));
-                    if (command === 'hard') {
-                        await orm.delete(donor)
-                        .where(inArray(donor.id, idList));
-                    } else {
-                        const status = command === 'reactivate';
-                        await orm.update(donor)
-                        .set({ active: status })
-                        .where(inArray(donor.id, idList));
-                    }
-                    return { success: true, message: `Done processing ${donorlist.length} donors` };
-                } catch (err: any) {
-                    console.error('try block failed on donor toggle: ' + err.message);
-                    return { success: false, message: err.message };
-                }
-}}
+        }
+    }
+}
