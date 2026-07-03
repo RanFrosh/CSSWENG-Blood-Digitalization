5d1b067f8e3a4a83ff8f88810e7e50e1619f0ba8
jetbuen <angel_buenaventura@dlsu.edu.ph>
2026-07-03 20:42:31 +0800

    Add text color to main container for error handling

--- sweng/app/mp/events/[eventId]/page.tsx
+++ sweng/app/mp/events/[eventId]/page.tsx
@@ -81,7 +81,7 @@ export default function MPEventPage() {
     // If the selected event is not found, display error message
     if (selectedEvent === undefined) {
         return (
-            <main className="flex flex-col min-h-screen bg-[#f9fdff]">
+            <main className="flex flex-col min-h-screen bg-[#f9fdff] text-black">
                 <Header />
 
                 <div className="flex-1 p-[0.35in]">
@@ -108,7 +108,7 @@ export default function MPEventPage() {
     // If the selected event is found, display event details and actions
     } else {
         return (
-            <main className="flex flex-col min-h-screen bg-[#f9fdff]">
+            <main className="flex flex-col min-h-screen bg-[#f9fdff] text-black">
                 <Header />
 
                 <div className="flex-1 p-[0.35in]">
