17271cf7ac42790e686c4936ccbbcb94f366d336
jetbuen <angel_buenaventura@dlsu.edu.ph>
2026-07-03 20:44:42 +0800

    Change main background color to include text-black

--- sweng/app/oa/events/[eventId]/page.tsx
+++ sweng/app/oa/events/[eventId]/page.tsx
@@ -56,7 +56,7 @@ export default function OAEventPage() {
     // If the selected event is not found, display error message
     if (selectedEvent === undefined) {
         return (
-            <main className="flex flex-col min-h-screen bg-[#f9fdff]">
+            <main className="flex flex-col min-h-screen bg-[#f9fdff] text-black">
                 <Header />
 
                 <div className="flex-1 p-[0.35in]">
@@ -83,7 +83,7 @@ export default function OAEventPage() {
     // If the selected event is found, display event details and actions
     } else {
         return (
-            <main className="flex flex-col min-h-screen bg-[#f9fdff]">
+            <main className="flex flex-col min-h-screen bg-[#f9fdff] text-black">
                 <Header />
 
                 <div className="flex-1 p-[0.35in]">
