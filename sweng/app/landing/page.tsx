d567cab75329fddcd0dce50d7ff339f485ba75a1
Metthy <adrian_matthew_dee@dlsu.edu.ph>
2026-07-07 13:20:53 +0800

    style: added text-black to landing page main

--- sweng/app/landing/page.tsx
+++ sweng/app/landing/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
     }, []);
 
     return (
-        <main className="flex flex-col min-h-screen bg-[#f9fdff]">
+        <main className="flex flex-col min-h-screen bg-[#f9fdff] text-black">
             <Header />
 
             <div className="flex-1 flex flex-row">
