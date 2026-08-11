4324f0f83c0c8ef13567237c6e9f6db36e0f7987
Hatzious <avtiu29@gmail.com>
2026-08-11 18:55:44 +0800

    chore: background preparations for incoming event_log feature

--- sweng/types/event_type.ts
+++ sweng/types/event_type.ts
@@ -1,6 +1,8 @@
 import { corrected_event } from "@/db/schemas/corrected_event";
 import { InferSelectModel, InferInsertModel } from "drizzle-orm";
 import { event_log } from "@/db/schemas/event_log";
+import { AccessType } from "@/db/enums/access_level";
+import { EventRecordAction } from "@/db/enums/event_action";
 
 export type ViewCorrections = InferSelectModel<typeof corrected_event>;
 export type ViewCorrectionFilters = Partial<ViewCorrections>;
@@ -16,3 +18,14 @@ export type ViewEventsWithProvince = ViewEvents & {
 export type ViewEventFilters = Partial<ViewEvents>;
 
 export type CreateEvents = InferInsertModel<typeof event_log>;
+
+type ViewEventRecords = {
+    event_log_id: bigint;
+    event_name: string;
+    staff_name: string;
+    staff_role: AccessType;
+    donor_first_name: string;
+    donor_last_name: string;
+    action: EventRecordAction;
+    time: string;  // "HH:MM:SS"
+};
