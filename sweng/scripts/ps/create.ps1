a277cb1e1360f2e47d63a0719ca7f567ebd25c89
Hatzious <avtiu29@gmail.com>
2026-08-13 15:48:59 +0800

    fix: updated script to have the cron job creation resolve to relative filepath

--- sweng/scripts/ps/create.ps1
+++ sweng/scripts/ps/create.ps1
@@ -1,1 +1,2 @@
-schtasks /Create /TN "RedBank Proj" /SC HOURLY /MO 1 /TR "E:\CSSWENG\Blood\sweng\scripts\cron\event_update.cmd" /F
+$cmdPath = (Resolve-Path (Join-Path $PSScriptRoot "..\cron\event_update.cmd")).Path
+schtasks /Create /TN "RedBank Proj" /SC HOURLY /MO 1 /TR "$cmdPath" /F
