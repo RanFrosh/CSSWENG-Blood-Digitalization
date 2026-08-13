204316e90b2ee122425546fd1c8845901a23f76c
Hatzious <avtiu29@gmail.com>
2026-08-13 15:44:57 +0800

    fix: adjusted to run cron job every hour

--- sweng/scripts/ps/create.ps1
+++ sweng/scripts/ps/create.ps1
@@ -1,1 +1,1 @@
-schtasks /Create /TN "RedBank Proj" /SC HOURLY /MO 1 /TR "E:\CSSWENG\Blood\sweng\scripts\cron\event_update.cmd" /F /RU "$env:USERDOMAIN\$env:USERNAME" /NP
+schtasks /Create /TN "RedBank Proj" /SC HOURLY /MO 1 /TR "E:\CSSWENG\Blood\sweng\scripts\cron\event_update.cmd" /F
