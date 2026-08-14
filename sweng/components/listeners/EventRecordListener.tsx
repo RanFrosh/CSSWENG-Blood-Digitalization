90f205a4534a9a322de944d6e66f37788ab3e6f8
Hatzious <avtiu29@gmail.com>
2026-08-14 20:05:41 +0800

    queue and event listeners for med prof and lab staff modified to use publishable key instead of a non-existent anon key

--- sweng/components/listeners/EventRecordListener.tsx
+++ sweng/components/listeners/EventRecordListener.tsx
@@ -11,7 +11,7 @@ export default function EventRecordListener({ eventId }: { eventId: string }) {
 
         const supabase = createBrowserClient(
             process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+            process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
         );
 
         const channel = supabase
