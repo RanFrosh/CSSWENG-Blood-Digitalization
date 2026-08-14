0b20e29c82d4068144fc96fe123e4767ddbf309c
Metthy <adrian_matthew_dee@dlsu.edu.ph>
2026-08-14 14:06:36 +0800

    feat: added EventRecordListener

---
+++ sweng/components/listeners/EventRecordListener.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createBrowserClient } from "@supabase/ssr";
+
+export default function EventRecordListener({ eventId }: { eventId: string }) {
+    const router = useRouter();
+
+    useEffect(() => {
+
+        const supabase = createBrowserClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        );
+
+        const channel = supabase
+            .channel('event_record-updates')
+            .on(
+                'postgres_changes',
+                {
+                    event: '*',
+                    schema: 'public',
+                    table: 'event_record',
+                    filter: `event_log_id=eq.${eventId}`
+                },
+                (payload) => {
+                    console.log("Event records updated", payload);
+                    router.refresh(); 
+                }
+            )
+            .subscribe();
+
+        return () => {
+            supabase.removeChannel(channel);
+        };
+    }, [eventId, router]);
+
+    return null;
+}
